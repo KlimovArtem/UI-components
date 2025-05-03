83ae897823d1fd8fac0e3888304e56b7c5566337
artemklimov <grinchomsk@gmail.com>
2025-05-03 07:33:42 +0600

    Add html markup with default styles; add js script for control open/close popover by button, by Esc key, by click out of element.

---
+++ origin_components/popover/popover.js
@@ -0,0 +1,41 @@
+document.querySelectorAll("[data-menu]").forEach(el => {
+  const openBtn = el.querySelector("[data-menu-open-btn]");
+  
+  let popover = document.querySelector("[data-popover]");
+  
+  openBtn.addEventListener("click", e => {
+    popover.showModal();
+
+    /* установка блокировки скрола */
+    document.body.classList.add("scroll-lock");
+  });
+});
+
+document.querySelectorAll("[data-popover]").forEach(popover => {
+  const closeButton = popover.querySelector("[data-popover__close-button]");
+  
+  closeButton.addEventListener("click", event => {
+    popover.close()
+  });
+  /* подтверждение закрытия через Esc*/
+  popover.addEventListener('cancel', (event) => {
+  event.preventDefault();
+  /* снятие блокировки скрола */
+  document.body.classList.remove("scroll-lock");
+})
+  
+  /*закрытие при щелчке вне модального окна */
+  popover.addEventListener("click", event => {
+    const popoverRect = popover.getBoundingClientRect();
+
+    if (
+      event.clientX < popoverRect.left ||
+      event.clientX > popoverRect.right ||
+      event.clientY < popoverRect.top ||
+      event.clientY > popoverRect.bottom
+    ) {
+      popover.close();
+      document.body.classList.remove("scroll-lock");
+    }
+  });
+});
