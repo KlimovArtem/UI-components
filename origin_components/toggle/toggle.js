2d12ccd66b0c877e694658fed68eb3c59cc097b4
artemklimov <grinchomsk@gmail.com>
2025-05-03 08:16:55 +0600

    Add script for keybord control: checked/unchecked by Enter key press; Add midificators for toggle size/

---
+++ origin_components/toggle/toggle.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("[data-toggle").forEach(toggle => {
+  toggle.addEventListener("keydown", event => {
+    if (event.key == "Enter"){
+      if (toggle.checked == false) {
+        toggle.checked = true;
+      } else {
+        toggle.checked = false;
+      }
+    }
+  })
+})
